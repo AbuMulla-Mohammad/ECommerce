11ef54b628e9dce7284b7a123bafc0fbef6c2a40
AbuMulla-Mohammad <m2872001@gmail.com>
2025-05-09 22:24:19 +0300

    feat: Add cart setup - Add Cart model - Add CartsController - Add cart migration

--- ECommerce.API/Data/ApplicationDbContext.cs
+++ ECommerce.API/Data/ApplicationDbContext.cs
@@ -10,8 +10,15 @@ namespace ECommerce.API.Data
         : base(options)
         {
         }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            builder.Entity<Cart>()
+                .HasKey(c => new { c.ProductId, c.ApplicationUserId });//primary composite key using fluent api
+        }
         public DbSet<Category> Categories { get; set; }
         public DbSet<Product> Products { get; set; }
         public DbSet<Brand>Brands { get; set; }
+        public DbSet<Cart> Carts { get; set; }
     }
 }
---
+++ ECommerce.API/Models/Cart.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ECommerce.API.Models
+{
+    //[PrimaryKey(nameof(ProductId),nameof(ApplicationUserId))]//primary composite key using data anotaion
+    public class Cart
+    {
+        public int ProductId { get; set; }
+        public Product Product { get; set; }
+        public string ApplicationUserId { get; set; }
+        public ApplicationUser ApplicationUser { get; set; }
+        public int Count { get; set; }
+    }
+}

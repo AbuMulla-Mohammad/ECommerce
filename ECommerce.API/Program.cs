0b0f71106a898baee313ab16f8364b285359c425
AbuMulla-Mohammad <m2872001@gmail.com>
2025-05-10 20:33:44 +0300

    feat: Implement email sending utility.
    - Created `EmailSender` class implementing `IEmailSender` for sending emails.
    - Integrated environment variables for email credentials (`EMAIL_USER` and `EMAIL_PASS`).
    - register the EmailSender service as Transient

--- ECommerce.API/Program.cs
+++ ECommerce.API/Program.cs
@@ -1,8 +1,11 @@
 
+using DotNetEnv;
 using ECommerce.API.Data;
 using ECommerce.API.Models;
 using ECommerce.API.Services;
+using ECommerce.API.Utility;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.EntityFrameworkCore;
 using Scalar.AspNetCore;
 
@@ -13,7 +16,7 @@ namespace ECommerce.API
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
-
+            Env.Load();
             // Add services to the container.
 
             builder.Services.AddControllers();
@@ -36,6 +39,7 @@ namespace ECommerce.API
             builder.Services.AddScoped<IBrandService,BrandService>();
             builder.Services.AddScoped<IProductService, ProductService>();
             builder.Services.AddScoped<ICartService, CartService>();
+            builder.Services.AddTransient<IEmailSender, EmailSender>();
             builder.Services.AddIdentity<ApplicationUser, IdentityRole>(options =>
             {
                 options.User.RequireUniqueEmail = false;
---
+++ ECommerce.API/Utility/EmailSender.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Identity.UI.Services;
+using System.Net.Mail;
+using System.Net;
+
+namespace ECommerce.API.Utility
+{
+    public class EmailSender : IEmailSender
+    {
+        public Task SendEmailAsync(string email, string subject, string message)
+        {
+            var senderEmail = Environment.GetEnvironmentVariable("EMAIL_USER");
+            var sebderPassword = Environment.GetEnvironmentVariable("EMAIL_PASS");
+            var client = new SmtpClient("smtp.gmail.com", 587)
+            {
+                EnableSsl = true,
+                UseDefaultCredentials = false,// use your own credentials not gmail credentials
+                Credentials = new NetworkCredential(senderEmail,sebderPassword)
+            };
+
+            return client.SendMailAsync(
+                new MailMessage(from: "m2872001@gmail.com",
+                                to: email,
+                                subject,
+                                message
+                                )
+                {
+                    IsBodyHtml= true,// this will allow you to send html content
+                }
+                );
+        }
+    }
+}
